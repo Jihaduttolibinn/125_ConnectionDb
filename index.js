fe013abf6327a866c8b05f67d673189df8c4f24c
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-15 11:48:14 +0700

    menambahkan parse JSON

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const PORT = process.env.PORT || 3000;
 
 
 const app = express();
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
+
 
 const db = mysql.createConnection({
     host: '127.0.0.1',
